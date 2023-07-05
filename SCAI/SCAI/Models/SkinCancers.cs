7eafafffe372a589ef961fef82785da70d7894c1
BOLOFFICIAL <boloninnn@gmail.com>
2023-07-06 00:25:53 +0300

    Добавлено распознавание и вывод результатор

--- SCAI/SCAI/Models/SkinCancers.cs
+++ SCAI/SCAI/Models/SkinCancers.cs
@@ -2,15 +2,21 @@
 {
     public class SkinCancers
     {
-        public static List<string> Cancers = new List<string>() 
+        private static Dictionary<string, string> Cancers = new Dictionary<string, string>()
         {
-            "Actinic keratoses and intraepithelial carcinoma / Bowen's disease",
-            "Basal cell carcinoma 6",
-            "Benign keratosis-like lesions (solar lentigines / seborrheic keratoses and lichen-planus like keratoses) 5",
-            "Dermatofibroma",
-            "Melanoma",
-            "Melanocytic nevi",
-            "Vascular lesions (angiomas, angiokeratomas, pyogenic granulomas and hemorrhage)",
+            {"akiec","Актинический кератоз и внутриэпителиальный карцинома / болезнь Боуэна"},
+            {"bcc","Базалиома"},
+            {"bkl","Доброкачественные кератозоподобные образования (солнечные лентиго / себорейные кератозы и лихено-подобные кератозы)"},
+            {"df","Дерматофиброма"},
+            {"mel","Меланома"},
+            {"nv","Меланоцитовые невусы"},
+            {"vasc","Сосудистые образования (ангиомы, ангиокератомы, пиогенные грануломы и кровотечения)"}
         };
+
+
+        public static string GetCancer(string key)
+        {
+            return Cancers[key];
+        }
     }
 }
